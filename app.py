e76455bb41b39eec1e53360281f844868462daef
Jan Kondratiuk <abg.projekt@wp.pl>
2021-03-18 21:00:32 +0100

    Calculator improved

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
-import get_rates
+import current_rates
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-rates = get_rates.rates_list()
+rates = current_rates.rates_list()
+currency_code = [[i[1], i[0]] for i in rates]
 
 @app.route("/calc/", methods=["GET", "POST"])
 def calc():
-    if request.method == "GET":
-        currency_code = [[i[1], i[0]] for i in rates]
-        return render_template("calculator.html", currency_code=currency_code)
     if request.method == "POST":
         input_data = request.form
         code = input_data.get('code')
-        how_much = input_data.get('how_much')
-        rate = [i[3] for i in rates if i[1] == code][0]
-        result = round(float(rate) * float(how_much), 2)
-        return render_template("result.html", result=result, rate=rate, code=code, how_much=how_much)
-
+        how_much = float(input_data.get('how_much'))
+        rate = float([i[3] for i in rates if i[1] == code][0])
+        result = round(rate * how_much, 2)
+        return render_template("result.html", result=f'{result:.2f}', rate=rate, code=code, how_much=f'{how_much:.2f}', currency_code=currency_code)
+    return render_template("calculator.html", currency_code=currency_code)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-import get_rates
+import current_rates
 
-rates = get_rates.rates_list()
+rates = current_rates.rates_list()
 currency_code = [i[1] for i in rates]
 #print(currency_code)
 
@@ -8,3 +8,7 @@ code = "GBP"
 
 match = [i[3] for i in rates if i[1] == code][0]
 print(match)
+
+result = f'{(round(1.3456 * 100000, 2)):.2f}'
+
+print (result)
